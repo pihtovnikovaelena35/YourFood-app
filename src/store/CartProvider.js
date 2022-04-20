6371231a5de8ac30a90c4769b28497e2c4398fa3
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2022-04-20 19:42:59 +0200

    Added a Cart Context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import CartContex from "./cart-context";
+
+const CartProvider = props => {
+    const addItemToCartHandler = item => { };
+
+    const removeItemFromCartHandler = id => { };
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+
+    return <CartContex.Provider value={cartContext}>
+        {props.children}
+    </CartContex.Provider>
+};
+
+export default CartProvider;
