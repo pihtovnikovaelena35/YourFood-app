92ff2ccaa84f248c638d26fd960e7f5b91c017e1
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2022-04-21 18:07:45 +0200

    Added Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,23 @@
+import { useContext } from 'react';
+
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContex from '../../../store/cart-context';
 
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
+
+    const cartCtx = useContext(CartContex);
+
+    const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
     return (
         <li className={classes.meal}>
@@ -13,7 +27,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-            <MealItemForm id={props.id} />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
