291b42808484c81f3b5adef0e1d82a39fa0851b7
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2022-06-03 21:16:28 +0200

    Sended Cart Data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,6 +25,16 @@ const Cart = (props) => {
         setIsCheckout(true);
     };
 
+    const submitOrderHandler = (userData) => {
+        fetch('https://food-order-app-http-ecc2b-default-rtdb.firebaseio.com/orders.json', {
+          method: 'POST',
+          body: JSON.stringify({
+            user: userData,
+            orderedItems: cartCtx.items
+          })
+        });
+      };
+
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) => (
@@ -56,7 +66,7 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
             {!isCheckout && modalActions}
         </Modal>
     );
